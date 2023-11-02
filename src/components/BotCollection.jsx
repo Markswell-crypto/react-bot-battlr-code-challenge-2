45c66db46c426ca9c5cc32c34ca2242651a76adb
Markswell <osoromarkswell80@gmail.com>
2023-11-02 20:49:22 +0300

    Fetching Bots using BotCollection Component

---
+++ src/components/BotCollection.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import BotCard from './BotCard';
+
+function BotCollection() {
+  const [bots, setBots] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from your API (http://localhost:3000/bots)
+    fetch('http://localhost:3000/bots')
+      .then((response) => response.json())
+      .then((data) => setBots(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Bot Collection</h1>
+      <div className="row">
+        {bots.map((bot) => (
+          <BotCard key={bot.id} bot={bot} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default BotCollection;
