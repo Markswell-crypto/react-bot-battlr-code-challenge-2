41062d3055a3dad474b447b3ff5274477eadc75a
Markswell <osoromarkswell80@gmail.com>
2023-11-02 20:44:58 +0300

    Minor Changes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import BotCollection from './BotCollection';
-import YourBotArmy from './YourBotArmy';
-import BotSpecs from './BotSpecs';
+import 'bootstrap'
+import BotCollection from './components/BotCollection';
+import BotArmy from './components/BotArmy';
+import BotSpecs from './components/BotSpecs';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <div className="container">
         <Switch>
           <Route path="/" exact component={BotCollection} />
-          <Route path="/your-bot-army" component={YourBotArmy} />
+          <Route path="/your-bot-army" component={BotArmy} />
           <Route path="/bot/:id" component={BotSpecs} />
         </Switch>
       </div>
