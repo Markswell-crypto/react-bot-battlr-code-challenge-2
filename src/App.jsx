ee8b2eb6d5a5cf0ae4571f1c2d1b284c8ccf2bd9
Markswell <osoromarkswell80@gmail.com>
2023-11-02 20:41:04 +0300

    Populating App.jsx Component Adding Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import BotCollection from './BotCollection';
+import YourBotArmy from './YourBotArmy';
+import BotSpecs from './BotSpecs';
 
 function App() {
-
   return (
-    <>
-      <div>
-        
+    <Router>
+      <div className="container">
+        <Switch>
+          <Route path="/" exact component={BotCollection} />
+          <Route path="/your-bot-army" component={YourBotArmy} />
+          <Route path="/bot/:id" component={BotSpecs} />
+        </Switch>
       </div>
-      
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
