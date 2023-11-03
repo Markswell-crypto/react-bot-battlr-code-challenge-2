f1b6c5dc61eded254f639ce8d1f570d3ca8e877a
Markswell <osoromarkswell80@gmail.com>
2023-11-03 13:49:43 +0300

    Adding Selected component to BootCollection

--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -2,9 +2,11 @@
 // a list of bot cards using the BotCard component.
 import React, { useState, useEffect } from 'react';
 import BotCard from './BotCard';
+import SelectedBots from './SelectedBots';
 
 function BotCollection() {
   const [bots, setBots] = useState([]);
+  const [selectedBots, setSelectedBots] = useState([]);
 
   useEffect(() => {
     // Fetch data from your API (http://localhost:3000/bots)
@@ -14,15 +16,33 @@ function BotCollection() {
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
+  const onSelectBot = (bot) => {
+    // Check if the bot is already selected
+    if (!selectedBots.find((b) => b.id === bot.id)) {
+      setSelectedBots([...selectedBots, bot]);
+    }
+  };
+
+  const removeBot = (bot) => {
+    const updatedBots = selectedBots.filter((b) => b.id !== bot.id);
+    setSelectedBots(updatedBots);
+  };
+
   return (
     <div className="container">
-        <br></br>
+      <br />
       <h1 className="bg-primary text-white text-center">Bot Collection</h1>
       <div className="row">
         {bots.map((bot) => (
-          <BotCard key={bot.id} bot={bot} />
+          <BotCard
+            key={bot.id}
+            bot={bot}
+            onSelectBot={onSelectBot}
+            selected={selectedBots.some((selectedBot) => selectedBot.id === bot.id)}
+          />
         ))}
       </div>
+      <SelectedBots selectedBots={selectedBots} removeBot={removeBot} />
     </div>
   );
 }
